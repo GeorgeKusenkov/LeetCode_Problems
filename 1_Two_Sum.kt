d38659a6186e011bdda32fa11272b83d9d2bb6e4
GeorgeKusenkov <g-jazzman@yandex.ru>
2024-01-12 13:43:39 +0500

    Update 1_Two_Sum.kt

--- 1_Two_Sum.kt
+++ 1_Two_Sum.kt
@@ -1,3 +1,10 @@
+fun main() {
+    val nums = intArrayOf(2,3,7,11)
+    val target = 10
+    val solution = Solution().twoSum(nums, target).contentToString()
+    println(solution)
+}
+
 class Solution {
     fun twoSum(nums: IntArray, target: Int): IntArray {
         val map = mutableMapOf<Int, Int>()
